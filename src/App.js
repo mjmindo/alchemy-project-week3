c5191cf856d240fc5545eec6799340f8b3852db6
Michael James Mindo <mmindo@centralizedinc.com>
2023-06-24 14:52:14 +0800

    Get block details and transactions list

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
+  network: Network.ETH_GOERLI,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [selectedOption, setSelectedOption] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -28,9 +22,69 @@ function App() {
     }
 
     getBlockNumber();
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+
+    try {
+      const blockDetails = await alchemy.core.getBlockWithTransactions(blockNumber);
+      console.log('blockDetails :>> ', blockDetails);
+      setSearchResult(blockDetails.transactions);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      Block Number: {blockNumber}
+
+      <div>
+        <br />
+        <select value={selectedOption} onChange={handleOptionChange}>
+          <option>Block Details</option>
+          <option>Transactions List</option>
+        </select>
+        <button onClick={handleSearch} disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Search'}
+        </button>
+      </div>
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {selectedOption === 'Block Details' && (
+            <div>
+              {searchResult.length > 0 && (
+                <div>
+                  <div>Block Number: {searchResult[0].blockNumber}</div>
+                  <div>Block Hash: {searchResult[0].blockHash}</div>
+                </div>
+              )}
+            </div>
+          )}
+
+          {selectedOption === 'Transactions List' && (
+            <div>
+              {searchResult.map((transaction, index) => (
+                <div key={index}>
+                  <a href={transaction.url}>{transaction.hash}</a>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
